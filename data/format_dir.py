2b2d1b97b8f1cb320fb74024ad0745c7242c9602
mrdung273 <doanvietdung273@gmail.com>
2019-04-01 16:25:52 +0700

    Modify path in format dir

--- data/format_dir.py
+++ data/format_dir.py
@@ -3,9 +3,7 @@ import pandas as pd
 import shutil
 
 
-def format_dir(train_val_dir="../dataset/train_and_val_images/",
-               target_dir="../dataset/train_images",
-               name_csv="../dataset/train.csv"):
+def format_dir(train_val_dir, target_dir, name_csv):
     # format dir into keras on-the-fly image generator format
 
     # train and val data frames
@@ -26,9 +24,9 @@ def format_dir(train_val_dir="../dataset/train_and_val_images/",
 
 
 if __name__ == '__main__':
-    format_dir(train_val_dir="../dataset/fungi",
+    format_dir(train_val_dir="../dataset/images",
                target_dir="../dataset/train_images",
-               name_csv="../dataset/train.csv")
-    format_dir(train_val_dir="../dataset/fungi",
+               name_csv="../dataset/train_val_annotations/train.csv")
+    format_dir(train_val_dir="../dataset/images",
                target_dir="../dataset/val_images",
-               name_csv="../dataset/val.csv")
+               name_csv="../dataset/train_val_annotations/val.csv")
